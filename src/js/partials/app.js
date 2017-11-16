c080da81bb8d716ada78ed96ca273ce47502c65a
DESKTOP-U2BHG16\Sponom <speedblizzz@yandex.ru>
2017-11-16 15:04:13 +0500

    other pages

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,4 +1,6 @@
 (function () {
+
+    $(document).ready(function(){
     var topMenuToggle = $('.top_main_menu_toggle'),
         topMenuWrap = $('.top_main_menu_wrap');
     topMenuToggle.on('click',function () {
@@ -22,8 +24,24 @@
         autoplaySpeed: 2000,
     });
 
-    $(document).ready(function(){
-        $('#lightgallery').lightGallery();
+    $('#lightgallery').lightGallery();
+
+    $('.testimonials_spoiler_title').on('click',function () {
+        $(this).next('.testimonials_spoiler_wrap').toggleClass('opened').slideToggle();
+
+        if($(this).next('.testimonials_spoiler_wrap').hasClass('opened')) {
+            var new_html = $(this).html().replace("spoiler_1", "spoiler_2");
+            new_html = new_html.replace("Читать", "Скрыть");
+            $(this).html(new_html);
+        }
+        else {
+            var new_html = $(this).html().replace("spoiler_2", "spoiler_1");
+            new_html = new_html.replace("Скрыть", "Читать");
+            $(this).html(new_html);
+        }
+
+    })
+
     });
 
 
